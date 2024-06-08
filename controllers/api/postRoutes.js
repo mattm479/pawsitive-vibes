c9e5c60fe852cfacc7c00af83c245c08ec562afe
pchoi87 <Choi.phil87@gmail.com>
2024-06-08 19:06:31 -0400

    Merge branch 'main' of https://github.com/mattm479/pawsitive-vibes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const session = require('express-session');
+const {Post} = require('../../models');
+
+router.post('/', async (req, res) => {
+try {
+     const newPost = await Post.create({
+        ...req.body,
+        user_id: req.session.user_id,
+     })
+     res.status(200).json(newPost);
+} catch (error) {
+   res.status(400)
+   if(newPost) {
+    res.status(400).json({ messege: 'no post found with id'});
+    return;
+   }}
+
+
+})
+
+
+
+
+
+
+module.exports = router;
