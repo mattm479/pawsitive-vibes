4433bb2eeab89e5d662c3e39d83e3c2a5f0188b5
Matthew Miskov <mattm479@gmail.com>
2024-06-10 20:25:13 -0400

    Updating Routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,8 @@
-const router = require('express').Router();
-const {Post} = require('../../models');
+    const router = require('express').Router();
+const { Post, User} = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
       ...req.body,
@@ -9,6 +10,30 @@ router.post('/', async (req, res) => {
     })
     res.status(200).json(newPost);
   } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+});
+
+router.get('/feed', withAuth, async (req, res) => {
+  try {
+    const user = await User.findByPk(req.session.user_id);
+    const posts = await Post.findAll({
+      where: {
+        category: user.favorite_animals
+      },
+      exclude: {
+        user_id: req.session.user_id
+      }
+    });
+
+    if (!posts) {
+      res.status(404).json('No posts found');
+    }
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
     res.status(400).json(error);
   }
 });
