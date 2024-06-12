166027ee529112c10f94de4d4acc6e7f0b53ad87
Matthew Miskov <mattm479@gmail.com>
2024-06-12 19:36:34 -0400

    Changing home route to render login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage', { user: req.session.user });
+    res.render('login', { user: req.session.user });
   } catch (err) {
     res.status(500).json(err);
   }
